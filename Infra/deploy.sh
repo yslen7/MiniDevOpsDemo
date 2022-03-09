892d21e5ab4cc51b8054336b8b4809a3a9816a2b
YslenGonzalez <yslen.gonzalez@improving.com>
2022-03-08 21:55:41 -0600

    First commit

---
+++ Infra/deploy.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# --- Variables ---
+Bucket="s3-demo-yslen"
+Tag="demo-yslen"
+ClusterName="demo-yslen-ecs"
+DNSName="demo.yslen.com"
+GitBranch="main"
+SecurityGroupName="demo-yslen-sg"
+LoadBalancerName="demo-yslen-lb"
+
+# --- Deploy ---
+aws cloudformation deploy \
+--stack-name ${Tag}-env \
+--template-file env.yml \
+--capabilities CAPABILITY_NAMED_IAM \
+--s3-bucket ${Bucket} \
+--parameter-overrides Tag=${Tag} \
+ClusterName=${ClusterName} \
+DNSName=${DNSName} \
+GitBranch=${GitBranch} \
+SecurityGroupName=${SecurityGroupName} \
+LoadBalancerName=${LoadBalancerName}
